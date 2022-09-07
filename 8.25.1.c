823b76fcbe571eb8738e341f8ce607e6f561a30e
wumingyyds <112532581+wumingyyds@users.noreply.github.com>
2022-09-07 12:45:38 +0800

    Add files via upload

---
+++ 8.25.1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	int b;
+	printf("请输入数字：",a);
+	scanf("%d",&a);
+	b=a%2;
+	if(a<0||a>100)
+	printf("数字不在范围");
+	else 
+	{
+		if(b==1)
+	{
+		printf("数字为奇数：%d\n",a);
+	} 
+	else 
+	{
+		printf("数字为偶数：%d\n",a);
+	} 
+	}
+	return 0;
+}
